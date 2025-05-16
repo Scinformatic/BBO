61d5b554030f581a0b53e22e171aabbe8a0e53e8
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-16 07:02:12 +0200

    Update __init__.py

--- pkg/src/bbo/__init__.py
+++ pkg/src/bbo/__init__.py
@@ -1,6 +1,45 @@
 """BBO: Bounding Box Optimization Algorithms."""
 
+from __future__ import annotations
 
-from bbo.approx_hull import approx_hull
+from typing import TYPE_CHECKING
 
-__all__ = ["approx_hull"]
+import numpy as np
+import jax.numpy as jnp
+
+from bbo import hull, pca
+
+if TYPE_CHECKING:
+    from typing import Literal
+    from numpy.typing import ArrayLike
+    from bbo.output import BBOOutput
+
+__all__ = ["run", "hull", "pca"]
+
+
+def run(points: ArrayLike, method: Literal["hull", "pca", "best"] = "best") -> BBOOutput:
+    if method == "hull":
+        return hull.run(points)
+    if method == "pca":
+        return pca.run(points)
+    if method == "best":
+        hull_output = hull.run(points)
+        pca_output = pca.run(points)
+        if hull_output.volume.ndim == 0:
+            return hull_output if hull_output.volume < pca_output.volume else pca_output
+        points = np.empty(hull_output.points.shape)
+        box = np.empty(hull_output.box.shape)
+        rotation = np.empty(hull_output.rotation.shape)
+        volume = np.empty(hull_output.volume.shape)
+        for i in range(hull_output.points.shape[0]):
+            selected = hull_output if hull_output.volume[i] < pca_output.volume[i] else pca_output
+            points[i] = selected.points[i]
+            box[i] = selected.box[i]
+            rotation[i] = selected.rotation[i]
+            volume[i] = selected.volume[i]
+        return BBOOutput(
+            points=jnp.asarray(points),
+            box=jnp.asarray(box),
+            rotation=jnp.asarray(rotation),
+            volume=jnp.asarray(volume)
+        )
