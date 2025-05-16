4cb9377570947bd0aa5a1ec2b46aadb92382e0f8
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-16 03:43:45 +0200

    Update output.py

--- pkg/src/bbo/output.py
+++ pkg/src/bbo/output.py
@@ -4,13 +4,25 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from jax import Array
+    from jax.numpy import ndarray
 
 
 @dataclass
 class BBOOutput:
-    """Bounding box optimization output."""
-    rotation: Array
-    box: Array
-    points: Array
-    volume: float | Array
+    """Bounding box optimization output.
+
+    Attributes
+    ----------
+    points
+        Rotated points in minimal bounding box alignment.
+    box
+        Corners of the minimal bounding box in the original coordinate system.
+    rotation
+        Rotation matrix aligning points to minimal bounding box axes.
+    volume
+        Volume of the minimal bounding box.
+    """
+    points: ndarray
+    box: ndarray
+    rotation: ndarray
+    volume: ndarray
