61c36cb2c1a2b95501e6e883280454570e303093
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-16 12:54:11 +0200

    Update hull.py

--- pkg/src/bbo/hull.py
+++ pkg/src/bbo/hull.py
@@ -1,3 +1,20 @@
+"""Optimize the oriented minimum-volume bounding box (MVBB) using convex hull method.
+
+For 2D points, the **rotating calipers** method is used to find the global minimum as follows:
+
+1. For each edge of the convex hull, consider that edge as the bottom edge of the bounding box.
+2. Rotate the points so that this edge is aligned with the x-axis.
+3. Compute the axis-aligned bounding box (AABB) of the rotated points.
+4. Compute the area of the AABB.
+5. Keep track of the minimum area found.
+6. Return the values that correspond to the minimum area.
+
+In 2D, the minimum-area bounding box has always one side aligned with an edge of the convex hull.
+Therefore, the algorithm is guaranteed to find the global minimum.
+On the other hand, in 3D, the minimum-volume bounding box can be aligned with
+a face normal, an edge direction, or a combination of both.
+"""
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
