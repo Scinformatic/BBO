a47525d975772910afba720ce54cb0dda478e70d
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-21 23:22:12 +0200

    Update hull.py

--- pkg/src/bbo/hull.py
+++ pkg/src/bbo/hull.py
@@ -1,8 +1,5 @@
 """Optimize the oriented minimum-volume bounding box (MVBB) using convex hull method."""
 
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
 import logging
 
 import ray
@@ -13,9 +10,7 @@ import scipy as sp
 
 from bbo import exception, util
 from bbo.output import BBOOutput
-
-if TYPE_CHECKING:
-    from numpy.typing import ArrayLike
+from bbo.typing import atypecheck, Array, JAXArray, Num, Integer
 
 
 __all__ = [
@@ -27,7 +22,8 @@ __all__ = [
 ]
 
 
-def run(points: ArrayLike) -> BBOOutput:
+@atypecheck
+def run(points: Num[Array, "*n_batches n_samples n_features"]) -> BBOOutput:
     """Calculate the oriented minimum-volume bounding box (MVBB) of a set of points (or batch thereof).
 
     Parameters
@@ -36,27 +32,43 @@ def run(points: ArrayLike) -> BBOOutput:
         Points as an array of shape `(n_points, n_dimensions)`
         or `(n_batches, n_points, n_dimensions)`.
     """
-    points = np.asarray(points)
-    if points.ndim == 2:
-        simplices = jnp.asarray(hull_simplices_single(points))
-        points = jnp.asarray(points)
-        func = run_single
-    elif points.ndim == 3:
-        simplices = hull_simplices_batch(points)
-        points = jnp.asarray(points)
-        func = run_batch
-    else:
+    if points.shape[-2] < 2:
         raise exception.InputError(
             name="points",
             value=points,
-            problem=f"Expected 2D or 3D input, but got shape {points.shape}"
+            problem=f"At least 2 points are required, but got {points.shape[0]}."
         )
-    return BBOOutput(*func(points, simplices))
+    if points.shape[-1] < 2:
+        raise exception.InputError(
+            name="points",
+            value=points,
+            problem=f"At least 2 features are required, but got {points.shape[1]}."
+        )
+    if points.ndim == 2:
+        simplices = hull_simplices_single(points)
+        return BBOOutput(*run_single(points, simplices))
+    elif points.ndim == 3:
+        simplices = hull_simplices_batch(points)
+        return BBOOutput(*run_batch(points, simplices))
+    points_reshaped = points.reshape(-1, *points.shape[-2:])
+    simplices = hull_simplices_batch(points_reshaped)
+    points_rotated, bbox_vertices, rotation_final, volume_final = run_batch(points_reshaped, simplices)
+    batch_shape = points.shape[:-2]
+    return BBOOutput(
+        points=points_rotated.reshape(points.shape),
+        box=bbox_vertices.reshape(*batch_shape, *bbox_vertices.shape[-2:]),
+        rotation=rotation_final.reshape(*batch_shape, *rotation_final.shape[-2:]),
+        volume=volume_final.reshape(*batch_shape),
+    )
 
 
-def hull_simplices_batch(points: np.ndarray, array_out: bool = True) -> list[np.ndarray] | jnp.ndarray:
+@atypecheck
+def hull_simplices_batch(
+    points: Num[Array, "n_batches n_samples n_features"],
+    array_out: bool = True,
+) -> list[np.ndarray] | Integer[Array, "n_batches nmax_simplices n_features"]:
     ray.init(ignore_reinit_error=True, logging_level=logging.WARNING)
-    futures = [_hull_simplices_single.remote(pc) for pc in points]
+    futures = [_hull_simplices_single.remote(batch) for batch in points]
     simplices_list = ray.get(futures)
     if not array_out:
         return simplices_list
@@ -70,7 +82,8 @@ def hull_simplices_batch(points: np.ndarray, array_out: bool = True) -> list[np.
     return jnp.asarray(simplices_padded)
 
 
-def hull_simplices_single(points: ArrayLike) -> np.ndarray:
+@atypecheck
+def hull_simplices_single(points: Num[Array, "n_samples n_features"]) -> Integer[Array, "n_simplices n_features"]:
     """Calculate the convex hull of a set of 3D points.
 
     References
@@ -83,15 +96,21 @@ def hull_simplices_single(points: ArrayLike) -> np.ndarray:
 
 
 @ray.remote
-def _hull_simplices_single(points: ArrayLike) -> np.ndarray:
+def _hull_simplices_single(points: Num[Array, "n_samples n_features"]) -> Integer[Array, "n_simplices n_features"]:
     return hull_simplices_single(points)
 
 
 @jax.jit
+@atypecheck
 def run_single(
-    points: jnp.ndarray,
-    simplices: jnp.ndarray
-) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+    points: Num[Array, "n_samples n_features"],
+    simplices: Integer[Array, "n_simplices n_features"],
+) -> tuple[
+    Num[JAXArray, "n_samples n_features"],
+    Num[JAXArray, "2**n_features n_features"],
+    Num[JAXArray, "n_features n_features"],
+    Num[JAXArray, ""],
+]:
     """Calculate the oriented minimum-volume bounding box (MVBB) of a set of points.
 
     Parameters
@@ -102,6 +121,8 @@ def run_single(
         Indices of points forming each triangular face (from ConvexHull)
         as an integer array of shape `(n_faces_or_edges, n_dimensions)`.
     """
+    points = jnp.asarray(points)
+    simplices = jnp.asarray(simplices)
     points_ndim = points.shape[-1]
 
     if points_ndim == 2:
@@ -208,4 +229,20 @@ def run_single(
     return best_points, best_bbox, best_rotation, best_volume
 
 
-run_batch = jax.jit(jax.vmap(run_single, in_axes=(0, 0)))
+@jax.jit
+@atypecheck
+def run_batch(
+    points: Num[Array, "n_batches n_samples n_features"],
+    simplices: Integer[Array, "n_batches n_simplices n_features"],
+) -> tuple[
+    Num[JAXArray, "n_batches n_samples n_features"],
+    Num[JAXArray, "n_batches 2**n_features n_features"],
+    Num[JAXArray, "n_batches n_features n_features"],
+    Num[JAXArray, "n_batches"],
+]:
+    points = jnp.asarray(points)
+    simplices = jnp.asarray(simplices)
+    return _run_batch(points, simplices)
+
+
+_run_batch = jax.vmap(run_single, in_axes=(0, 0))
