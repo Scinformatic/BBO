782dbb10b25c4be0236ad3098ad62bbe995384df
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-21 23:02:12 +0200

    Update __init__.py

--- pkg/src/bbo/__init__.py
+++ pkg/src/bbo/__init__.py
@@ -1,29 +1,27 @@
 """BBO: Bounding Box Optimization Algorithms."""
 
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
 import numpy as np
 import jax.numpy as jnp
+from typing import Literal
 
-from bbo import hull, pca
+from bbo import hull, pca, exception
 from bbo.output import BBOOutput
+from bbo.typing import atypecheck, Num, Array
 
-if TYPE_CHECKING:
-    from typing import Literal
-    from numpy.typing import ArrayLike
 
 __all__ = ["run", "hull", "pca"]
 
 
-def run(points: ArrayLike, method: Literal["hull", "pca", "best"] = "best") -> BBOOutput:
+@atypecheck
+def run(
+    points: Num[Array, "*n_batches n_samples n_features"],
+    method: Literal["hull", "pca", "best"] = "best",
+) -> BBOOutput:
     if method == "hull":
         return hull.run(points)
     if method == "pca":
         return pca.run(points)
     if method == "best":
-        points = np.asarray(points)
         if points.shape[-1] == 2:
             # Exact solution for 2D points
             return hull.run(points)
@@ -31,19 +29,26 @@ def run(points: ArrayLike, method: Literal["hull", "pca", "best"] = "best") -> B
         pca_output = pca.run(points)
         if hull_output.volume.ndim == 0:
             return hull_output if hull_output.volume < pca_output.volume else pca_output
-        points = np.empty(hull_output.points.shape, dtype=hull_output.points.dtype)
-        box = np.empty(hull_output.box.shape, dtype=hull_output.box.dtype)
-        rotation = np.empty(hull_output.rotation.shape, dtype=hull_output.rotation.dtype)
-        volume = np.empty(hull_output.volume.shape, dtype=hull_output.volume.dtype)
-        for i in range(hull_output.points.shape[0]):
-            selected = hull_output if hull_output.volume[i] < pca_output.volume[i] else pca_output
-            points[i] = selected.points[i]
-            box[i] = selected.box[i]
-            rotation[i] = selected.rotation[i]
-            volume[i] = selected.volume[i]
+        hull_is_better = hull_output.volume < pca_output.volume
+        mask_shape = hull_is_better.shape
+        mask_ndim = hull_is_better.ndim
+        points_rotated, box, rotation, volume = [
+            jnp.where(
+                hull_is_better.reshape(
+                    *mask_shape, *(1,) * (getattr(hull_output, attr_name).ndim - mask_ndim)
+                ),
+                getattr(hull_output, attr_name),
+                getattr(pca_output, attr_name),
+            ) for attr_name in ("points", "box", "rotation", "volume")
+        ]
         return BBOOutput(
-            points=jnp.asarray(points),
-            box=jnp.asarray(box),
-            rotation=jnp.asarray(rotation),
-            volume=jnp.asarray(volume)
+            points=points_rotated,
+            box=box,
+            rotation=rotation,
+            volume=volume,
         )
+    raise exception.InputError(
+        name="method",
+        value=method,
+        problem=f"Method '{method}' is not recognized. Use 'hull', 'pca', or 'best'."
+    )
